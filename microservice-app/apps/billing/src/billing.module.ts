9685550c90d0ac90ea6dfe5f4c6930acbbd384d1
rumeone <yuaroslav_29@mail.ru>
2023-08-23 00:58:30 +0700

    added microservices billing and configured module

--- microservice-app/apps/billing/src/billing.module.ts
+++ microservice-app/apps/billing/src/billing.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import {RmqModule} from "@app/common";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      RABBIT_MQ_URI: Joi.string().required(),
+      RABBIT_MQ_BILLING_QUEUE: Joi.string().required()
+    }),
+    envFilePath: './apps/billing/.env'
+  }) , RmqModule],
   controllers: [BillingController],
   providers: [BillingService],
 })
